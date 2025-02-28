bea38299b446b658aa1cfd55be4cc9e6d1e78d59
Jumana <Jumanarawashdeh10@gmail.com>
2025-02-28 22:53:31 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 
 
 void main() async {
   
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  await Hive.openBox<bool>('favorites'); 
+
   runApp(MaterialApp(home: const Scaffold()));
 }
 
